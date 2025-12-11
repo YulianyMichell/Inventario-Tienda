cc72520a7ca61c0ed7cbdbc78a5a7957fd41255c
Hannah02-07 <cabaricosalazarhannah@gmail.com>
2025-12-11 16:51:15 -0500

    Update web.php
    cambios en las rutas, las rutas del inventario

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InventarioController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Inventario
+Route::get('/inventario', [InventarioController::class, 'index'])->name('inventario.index');
+
+Route::get('/inventario/entrada', [InventarioController::class, 'createEntrada'])->name('inventario.createEntrada');
+Route::post('/inventario/entrada', [InventarioController::class, 'storeEntrada'])->name('inventario.storeEntrada');
+
+Route::get('/inventario/salida', [InventarioController::class, 'createSalida'])->name('inventario.createSalida');
+Route::post('/inventario/salida', [InventarioController::class, 'storeSalida'])->name('inventario.storeSalida');
+
 
 require __DIR__.'/auth.php';
